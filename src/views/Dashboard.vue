eb8002a8ce677ff0bb8dae3a9d9b6ee04d7906f9
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-07-31 18:25:02 +0700

    add home page

--- src/views/Dashboard.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <app> Dashboard </app>
-</template>
-
-<script setup>
-import { App } from '@/layouts';
-</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <app>
+    <div class="flex items-center justify-between mb-4">
+      <h1 class="font-bold text-2xl">All Todo</h1>
+      <home-action />
+    </div>
+    <todo-list />
+  </app>
+</template>
+
+<script setup>
+import { App } from '@/layouts';
+import { HomeAction } from '@/components/home';
+import { TodoList } from '@/components/todo';
+</script>
