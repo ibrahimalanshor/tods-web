a2ff0b22f704d715b7de85b12b1f2c17f88551b0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-07-31 15:36:48 +0700

    add sidebar component

---
+++ src/components/sidebar/render/SidebarNav.js
@@ -0,0 +1,184 @@
+import { ref, h } from 'vue';
+import { Icon as IconUtil } from '@vicons/utils';
+import { ChevronDown as ChevronDownIcon } from '@/components/icon';
+
+export default {
+  props: {
+    navs: Array,
+    active: {
+      type: Array,
+      default: [],
+    },
+    show: {
+      type: Array,
+      default: [],
+    },
+  },
+  emits: ['item-click'],
+  setup(props, { emit }) {
+    const active = ref(props.active);
+    const show = ref(props.show);
+
+    const handleItemClick = (item) => {
+      if (item.children?.length) {
+        if (!show.value.includes(item.key)) {
+          show.value.push(item.key);
+          active.value.push(item.key);
+        } else {
+          show.value = show.value.filter((value) => value !== item.key);
+          active.value = active.value.filter((value) => value !== item.key);
+        }
+      }
+
+      emit('item-click', item);
+    };
+    const handleChildClick = (item, e) => {
+      emit('item-click', item);
+    };
+
+    const renderArrow = (item) => {
+      return h(
+        ChevronDownIcon,
+        {
+          class: [
+            'item-arrow',
+            item.showChildren || show.value.includes(item.key)
+              ? ' rotate-180'
+              : '',
+          ],
+        },
+        {}
+      );
+    };
+
+    const renderIcon = (icon) => {
+      return h(
+        IconUtil,
+        {},
+        {
+          default: () => h(icon, {}, {}),
+        }
+      );
+    };
+
+    const renderLabel = (item) => {
+      const activeClass = 'bg-primary-500 text-white font-medium';
+      const labelClass =
+        'px-4 flex items-center justify-between hover:bg-primary-500 hover:text-white hover:font-medium py-2 rounded cursor-pointer';
+
+      return h(
+        'div',
+        {
+          class: [
+            labelClass,
+            item.active || active.value.includes(item.key) ? activeClass : '',
+          ],
+          onClick: () => handleItemClick(item),
+        },
+        {
+          default: () => [
+            h(
+              'div',
+              { class: 'flex items-center space-x-4' },
+              {
+                default: () => [
+                  renderIcon(item.icon),
+                  h('span', {}, { default: () => item.label }),
+                ],
+              }
+            ),
+            ...(item.children?.length ? [renderArrow(item)] : []),
+          ],
+        }
+      );
+    };
+
+    const renderChild = (child, options) => {
+      const activeClass = 'bg-primary-100 text-primary-500 font-medium';
+      const labelClass =
+        'px-4 mb-2 flex items-center space-x-4 hover:bg-primary-100 hover:text-primary-500 hover:font-medium py-2 rounded cursor-pointer';
+
+      return h(
+        'div',
+        {
+          class: [
+            labelClass,
+            child.active || active.value.includes(child.key) ? activeClass : '',
+            options.last ? 'mb-0' : '',
+          ],
+          onClick: (e) => handleChildClick(child, e),
+        },
+        {
+          default: () => [
+            renderIcon(child.icon),
+            h('span', {}, { default: () => child.label }),
+          ],
+        }
+      );
+    };
+
+    const renderChildren = (children, options) => {
+      return h(
+        'div',
+        { class: ['bg-primary-50', 'py-1', options.show ? '' : 'hidden'] },
+        {
+          default: () =>
+            children.map((child, index) =>
+              renderChild(child, { last: index === children.length - 1 })
+            ),
+        }
+      );
+    };
+
+    const renderItem = (item) => {
+      return h(
+        'div',
+        { class: 'mb-2' },
+        {
+          default: () => [
+            renderLabel(item),
+            ...[
+              item.children?.length
+                ? renderChildren(item.children, {
+                    show: item.showChildren || show.value.includes(item.key),
+                  })
+                : [],
+            ],
+          ],
+        }
+      );
+    };
+
+    const renderTitle = (title) => {
+      return h(
+        'div',
+        { class: 'px-4 font-bold text-sm text-gray-600 uppercase mb-4' },
+        {
+          default: () => title,
+        }
+      );
+    };
+
+    const renderNav = (nav) => {
+      return h(
+        'div',
+        { class: 'mt-6 mb-4 px-4' },
+        {
+          default: () => [
+            renderTitle(nav.title),
+            ...nav.items.map((item) => renderItem(item)),
+          ],
+        }
+      );
+    };
+
+    return () =>
+      h(
+        'div',
+        {},
+        {
+          default: () => props.navs.map((nav) => renderNav(nav)),
+        }
+      );
+  },
+};
