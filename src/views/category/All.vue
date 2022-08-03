41b2d1ad24ae0bcebeb657412a5a795acaf39cf0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-03 21:42:33 +0700

    add category view

---
+++ src/views/category/All.vue
@@ -0,0 +1,27 @@
+<template>
+  <app>
+    <div class="flex items-center justify-between mb-4">
+      <h1 class="font-bold text-2xl">All Category</h1>
+      <category-list-action />
+    </div>
+    <category-list :categories="categories" />
+  </app>
+</template>
+
+<script setup>
+import { App } from '@/layouts';
+import { CategoryList } from '@/components/category';
+import { CategoryListAction } from '@/components/category/list';
+
+const categories = [
+  {
+    name: 'Works',
+  },
+  {
+    name: 'Schools',
+  },
+  {
+    name: 'Habbits',
+  },
+];
+</script>
