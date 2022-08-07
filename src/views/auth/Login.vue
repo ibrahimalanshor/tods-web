804b98c3e602ea246fcfc33b632c2eeef8dd8e5a
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-07 21:17:23 +0700

    add process login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -5,9 +5,30 @@
         Login
       </h1>
 
-      <div class="">
-        <ui-form-item label="Email" placeholder="Email" />
-        <ui-form-item label="Password" placeholder="Password" />
+      <ui-alert
+        class="mb-4"
+        color="danger"
+        :text="loginAlert.message"
+        :visible="loginAlert.visible"
+        v-on:close="handleAlertClose"
+      />
+
+      <form v-on:submit.prevent="handleSubmitLogin">
+        <ui-form-item
+          label="Email"
+          placeholder="Email"
+          v-model="body.email"
+          :status="formError.email ? 'error' : ''"
+          :feedback="formError.email?.msg"
+        />
+        <ui-form-item
+          label="Password"
+          type="password"
+          placeholder="Password"
+          v-model="body.password"
+          :status="formError.password ? 'error' : ''"
+          :feedback="formError.password?.msg"
+        />
         <div class="flex justify-between mb-3">
           <ui-checkbox label="Remember Me" />
           <ui-link
@@ -15,8 +36,15 @@
             label="Forgot Password"
           />
         </div>
-        <ui-button color="primary" block class="mb-2">Sign In</ui-button>
-        <ui-button block class="mb-3">
+        <ui-button
+          type="submit"
+          color="primary"
+          block
+          class="mb-2"
+          :disabled="loading.items.login"
+          >Sign In</ui-button
+        >
+        <ui-button type="button" block class="mb-3">
           <span class="flex items-center space-x-2">
             <icon>
               <logo-google-icon />
@@ -28,14 +56,57 @@
           Doesn't have account?
           <ui-link :href="{ name: 'Register' }" label="Register Here" />
         </p>
-      </div>
+      </form>
     </div>
   </auth>
 </template>
 
 <script setup>
+import { computed, reactive } from 'vue';
 import { Auth } from '@/layouts';
-import { UiFormItem, UiButton, UiCheckbox, UiLink } from '@/components/ui';
+import {
+  UiFormItem,
+  UiButton,
+  UiCheckbox,
+  UiLink,
+  UiAlert,
+} from '@/components/ui';
 import { Icon } from '@vicons/utils';
 import { LogoGoogle as LogoGoogleIcon } from '@vicons/ionicons5';
+import { useError, useLoading, useToast } from '@/store';
+import { useLogin } from '@/compose/auth';
+import { HandledError } from '@/utils';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const error = useError();
+const loading = useLoading();
+const toast = useToast();
+const { body, login } = useLogin();
+
+const loginAlert = computed(() => {
+  return {
+    visible: error.errors.login?.status === 401,
+    message: error.errors.login?.errors,
+  };
+});
+const formError = computed(() => {
+  return error.errors.login?.errors ?? {};
+});
+
+const handleSubmitLogin = async () => {
+  try {
+    await login();
+
+    error.reset('login');
+    router.push({ name: 'Home' });
+  } catch (err) {
+    if (!(err instanceof HandledError)) {
+      toast.show('something error');
+    }
+  }
+};
+const handleAlertClose = () => {
+  error.reset('login');
+};
 </script>
