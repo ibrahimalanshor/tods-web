8e6ff86d63ce79b5739fba11ba91a5be2945267e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-08 21:59:41 +0700

    add category create component

--- src/components/category/Create.vue
+++ src/components/category/Create.vue
@@ -4,20 +4,85 @@
       <ui-button title="New Category" color="primary" />
     </template>
 
-    <category-form />
+    <ui-alert
+      class="mb-4"
+      color="danger"
+      :text="formAlert.message"
+      :visible="formAlert.visible"
+      v-on:close="handleAlertClose"
+    />
+
+    <category-form
+      :errors="formError"
+      v-model="categoryBody"
+      v-on:submit="handleFormSubmit"
+    />
 
     <template #footer>
-      <ui-button color="primary">Save</ui-button>
+      <ui-button
+        color="primary"
+        v-on:click="handleFormSubmit"
+        :disabled="loading.get('create-category')"
+        >Save</ui-button
+      >
       <ui-button v-on:click="hideCategoryModal">Cancel</ui-button>
     </template>
   </ui-modal>
 </template>
 
 <script setup>
-import { UiButton, UiModal } from '@/components/ui';
+import { watch, computed, reactive, inject } from 'vue';
+import { UiButton, UiModal, UiAlert } from '@/components/ui';
 import { CategoryForm } from '@/components/category';
-import { useCategoryModal } from '@/compose/category';
+import {
+  useCategoryModal,
+  useCategoryForm,
+  useCategoryCreate,
+} from '@/compose/category';
+import { useError, useLoading, useToast } from '@/store';
+import { HandledError } from '@/utils';
+
+const emitter = inject('emitter');
+const loading = useLoading();
+const error = useError();
 
 const { categoryModalVisible, showCategoryModal, hideCategoryModal } =
   useCategoryModal();
+const { categoryBody, setCategoryBody, resetCategoryBody } = useCategoryForm();
+const { createCategory } = useCategoryCreate();
+
+const formAlert = reactive({
+  visible: false,
+  message: '',
+});
+
+const formError = computed(() => {
+  return error.get('create-category')?.errors ?? {};
+});
+
+const handleFormSubmit = async () => {
+  try {
+    await createCategory(categoryBody.value);
+
+    emitter.emit('category-created', { msg: 'Category Created' });
+
+    hideCategoryModal();
+  } catch (err) {
+    if (!(err instanceof HandledError)) {
+      formAlert.visible = true;
+      formAlert.message = 'something error';
+    }
+  }
+};
+
+const handleAlertClose = () => {
+  formAlert.visible = false;
+};
+
+watch(categoryModalVisible, () => {
+  error.reset('create-category');
+  loading.stop('create-category');
+
+  resetCategoryBody();
+});
 </script>
--- src/components/category/Form.vue
+++ src/components/category/Form.vue
@@ -1,25 +1,43 @@
 <template>
-  <form>
+  <form v-on:submit.prevent="handleFormSubmit">
     <ui-form-item
       label="Name"
       type="text"
       id="name"
       placeholder="Name"
       v-model="body.name"
+      :status="errors.name ? 'error' : ''"
+      :feedback="errors.name?.msg"
     />
   </form>
 </template>
 
 <script setup>
-import { reactive } from 'vue';
+import { reactive, watch } from 'vue';
 import { UiFormItem } from '@/components/ui';
 
 const props = defineProps({
   modelValue: Object,
+  errors: {
+    type: Object,
+    default: () => {},
+  },
 });
-const emit = defineEmits(['update:modelValue']);
+const emit = defineEmits(['update:modelValue', 'submit']);
 
 const body = reactive({
   name: props.modelValue?.name ?? null,
 });
+
+const handleFormSubmit = () => {
+  emit('submit', body);
+};
+
+watch(
+  body,
+  () => {
+    emit('update:modelValue', body);
+  },
+  { deep: true }
+);
 </script>
