1f497a43d4e5953a85d178c5d32a5c2dbc752d8e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-09 16:20:40 +0700

    add category api

--- src/api/category.js
+++ src/api/category.js
@@ -2,7 +2,7 @@ import http from './http.js';
 
 export default {
   baseURL: '/categories',
-  get: async function ({ query } = {}) {
+  get: async function (query) {
     const res = await http().get(this.baseURL, {
       params: query,
     });
@@ -11,6 +11,16 @@ export default {
   },
   create: async function (body) {
     const res = await http().post(this.baseURL, body);
+
+    return res.data;
+  },
+  update: async function (id, body) {
+    const res = await http().patch(`${this.baseURL}/${id}`, body);
+
+    return res.data;
+  },
+  delete: async function (id) {
+    const res = await http().delete(`${this.baseURL}/${id}`);
 
     return res.data;
   },
