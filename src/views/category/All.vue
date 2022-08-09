c53d797240c37a4648cfab109d3a90ba777fca38
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-09 16:22:10 +0700

    finish category page

--- src/views/category/All.vue
+++ src/views/category/All.vue
@@ -2,36 +2,48 @@
   <app>
     <div class="flex items-center justify-between mb-4">
       <h1 class="font-bold text-2xl">All Category</h1>
-      <category-list-action />
+      <category-list-action :filter="filter" v-on:filter="handleFilter" />
     </div>
     <template v-if="!loading.get('get-category')">
-      <category-list :categories="categories.rows" />
+      <category-list :categories="categories?.rows ?? []" />
     </template>
+    <ui-skeleton class="h-20" v-else />
   </app>
 </template>
 
 <script setup>
 import { onMounted, onBeforeMount, inject } from 'vue';
 import { App } from '@/layouts';
+import { UiSkeleton } from '@/components/ui';
 import { CategoryList } from '@/components/category';
 import { CategoryListAction } from '@/components/category/list';
 import { useCategoryList } from '@/compose/category';
 import { useLoading, useToast } from '@/store';
+import { HandledError } from '@/utils';
 
 const emitter = inject('emitter');
 const loading = useLoading();
 const toast = useToast();
-const { categories, getCategories } = useCategoryList();
+const { categories, filter, getCategories } = useCategoryList();
 
 const setCategories = async () => {
   try {
     await getCategories();
   } catch (err) {
-    console.log(err);
+    if (!(err instanceof HandledError)) {
+      toast.show('something error');
+    }
   }
 };
 
-emitter.on('category-created', (e) => {
+const handleFilter = ({ sort, order }) => {
+  filter.sort = sort;
+  filter.order = order;
+
+  setCategories();
+};
+
+emitter.on('refresh-category', (e) => {
   toast.show(e.msg, 'success');
 
   setCategories();
