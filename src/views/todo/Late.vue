13abcc2db43632dd8d5d29eb62663b0ba0ba7625
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-10 19:44:32 +0700

    finish todo late today page

--- src/views/todo/Late.vue
+++ src/views/todo/Late.vue
@@ -1,66 +1,70 @@
 <template>
   <app>
     <div class="flex items-center justify-between mb-4">
-      <h1 class="font-bold text-2xl">Late</h1>
-      <todo-list-action />
+      <h1 class="font-bold text-2xl">Late Todo</h1>
+      <todo-list-action
+        :filter="filter"
+        :filter-items="{ status: false, due: false }"
+        :create-button="false"
+        v-on:filter="handleFilter"
+      />
     </div>
-    <todo-list :todos="todos" />
+    <template v-if="!loading.get('get-todo')">
+      <todo-list :todos="todos?.rows ?? []" />
+    </template>
+    <ui-skeleton v-else />
   </app>
 </template>
 
 <script setup>
+import { onMounted, onBeforeMount, inject } from 'vue';
 import { App } from '@/layouts';
+import { UiSkeleton } from '@/components/ui';
 import { TodoListAction } from '@/components/todo/list';
 import { TodoList } from '@/components/todo';
+import { useLoading, useToast } from '@/store';
+import { useTodoList } from '@/compose/todo';
+import { HandledError } from '@/utils';
 
-const todos = [
-  {
-    name: 'Finish Semester Antara',
-    description:
-      'Standar Enkripsi Data adalah algoritme kunci simetris untuk enkripsi data elektroni',
-    done: false,
-    due: new Date('2022-08-20T07:00:00'),
-    category: {
-      id: 1,
-      name: 'School',
-    },
-    doneAt: null,
-    createdAt: new Date('2022-08-02T07:00:00'),
-    children: [
-      {
-        name: 'KRS-an',
-        done: true,
-      },
-      {
-        name: 'UTS',
-        done: true,
-      },
-      {
-        name: 'UAS',
-        done: false,
-      },
-    ],
-  },
-  {
-    name: 'Cook for Dinner',
-    description: null,
-    done: false,
-    due: new Date('2022-08-01T08:00:00'),
-    category: null,
-    doneAt: null,
-    createdAt: new Date('2022-08-01T07:00:00'),
-  },
-  {
-    name: 'Backend Roadmap',
-    description: null,
-    done: false,
-    due: null,
-    category: {
-      id: 2,
-      name: 'Roadmap',
-    },
-    doneAt: null,
-    createdAt: new Date('2022-07-21T07:00:00'),
-  },
-];
+const emitter = inject('emitter');
+const loading = useLoading();
+const toast = useToast();
+
+const { todos, filter, getTodos } = useTodoList();
+
+const setTodos = async () => {
+  try {
+    filter.late = new Date();
+
+    await getTodos();
+  } catch (err) {
+    if (!(err instanceof HandledError)) {
+      toast.show('something error');
+    }
+  }
+};
+
+const handleFilter = ({ sort, order, categoryId }) => {
+  filter.sort = sort;
+  filter.order = order;
+  filter.categoryId = categoryId;
+
+  setTodos();
+};
+
+emitter.on('refresh-todo', (e) => {
+  if (e?.msg) {
+    toast.show(e.msg, 'success');
+  }
+
+  setTodos();
+});
+
+onBeforeMount(() => {
+  loading.start('get-todo');
+});
+
+onMounted(() => {
+  setTodos();
+});
 </script>
