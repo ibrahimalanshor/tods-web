06e3d3bbc5da34c460b8429ed4c6f9927b651330
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-10 21:44:20 +0700

    handle error in error store

--- src/helpers/handle_error.js
+++ src/helpers/handle_error.js
@@ -1,14 +1,21 @@
+import { useRouter } from 'vue-router';
 import { useError } from '@/store';
 import { HandledError } from '@/utils';
 
 export default (name, err) => {
+  const router = useRouter();
   const error = useError();
 
   error.reset(name);
 
   if (err?.response) {
     const { status, data } = err.response;
-    const handledStatus = [422, 401];
+    const handledStatus = [422, 401, 409];
+
+    if (status === 404) {
+      console.log('NotFound');
+      router.push({ name: 'NotFound' });
+    }
 
     if (handledStatus.includes(status)) {
       error.add(name, {
